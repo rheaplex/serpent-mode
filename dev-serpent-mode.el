0c009817bd932ba43aec74382357cc71e24fdb79
Rob Myers <rob@robmyers.org>
2014-05-20 20:01:55 -0700

    Initial commit.

---
+++ dev-serpent-mode.el
@@ -0,0 +1,43 @@
+;;; dev-serpent-mode.el - Code to generate regular expressions for serpent-mode
+;;
+;; Copyright (C) 2014 Rob Myers <rob@robmyers.org>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Syntax highlighting
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; keywords
+
+(concat "\\<"
+	(regexp-opt '("code" "else" "if" "init" "return" "stop" "suicide" "while"))
+	"\\>")
+
+;; Variables
+
+(concat "\\<"
+	(regexp-opt '("block.prevhash" "block.number" "block.timestamp"
+                  "block.difficulty" "block.coinbase" "block.gaslimit"
+                  "tx.gasprice" "tx.origin" "tx.gas" "msg.datasize"
+                  "msg.sender" "msg.value" "contract.balance" "msg.data"
+                  "contract.storage"))
+	"\\>")
+
+;; Functions
+
+(concat "\\<\\(" 
+        (regexp-opt '("array" "byte" "bytes" "create" "getch" "msg"
+                      "send" "setch" "sha3"))
+	"\\)\\s-*(")
---
+++ serpent-mode.el
@@ -0,0 +1,301 @@
+;;; serpent-mode.el - Ethereum Serpent major mode
+;;
+;; Copyright (C) 2014 Rob Myers <rob@robmyers.org>
+;; Indentation code from python.el
+;; Copyright (C) 2003-2013 Free Software Foundation, Inc.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Installation
+;;
+;; To install, add this file to your Emacs load path.
+;; You can then load it using M-x serpent-mode
+;; Alternatively you can have Emacs load it automatically for files with
+;; a .strike extension by adding the following to your .emacs file:
+;; 
+;;    (require 'serpent-mode)
+;;    (add-to-list 'auto-mode-alist '("\\.se$" . serpent-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Syntax highlighting
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconst serpent-font-lock-keywords
+  '(
+    ;; Keywords
+    ("\\<\\(?:code\\|else\\|i\\(?:f\\|nit\\)\\|return\\|s\\(?:top\\|uicide\\)\\|while\\)\\>"
+     . font-lock-keyword-face)
+    ;; Built-in variables
+    ("\\<\\(?:block\\.\\(?:coinbase\\|difficulty\\|gaslimit\\|number\\|prevhash\\|timestamp\\)\\|contract\\.\\(?:\\(?:balanc\\|storag\\)e\\)\\|msg\\.\\(?:data\\(?:size\\)?\\|sender\\|value\\)\\|tx\\.\\(?:gas\\(?:price\\)?\\|origin\\)\\)\\>"
+     . font-lock-variable-name-face)
+    ;; Built-in-functions
+    ("\\<\\(\\(?:array\\|bytes?\\|create\\|getch\\|msg\\|s\\(?:e\\(?:nd\\|tch\\)\\|ha3\\)\\)\\)\\s-*("
+     . font-lock-builtin-face)
+  )
+  "Highlighting for Serpent major mode.")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Indentation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defcustom serpent-indent-offset 4
+  "Default indentation offset for Serpent."
+  :group 'serpent
+  :type 'integer
+  :safe 'integerp)
+
+(defcustom serpent-indent-trigger-commands
+  '(indent-for-tab-command yas-expand yas/expand)
+  "Commands that might trigger a `serpent-indent-line' call."
+  :type '(repeat symbol)
+  :group 'serpent)
+
+(defvar serpent-indent-current-level 0
+  "Current indentation level `serpent-indent-line-function' is using.")
+
+(defvar serpent-indent-levels '(0)
+  "Levels of indentation available for `serpent-indent-line-function'.")
+
+(defconst serpent-block-start "^\\s-*\\(init\\|code\\|if\\|while\\)"
+  "A regex to match the starts of blocks for serpent-indent-calculate-indentation")
+
+;; FIXME: de-indent after functions that end execution
+;;(defconst serpent-block-start "^\\s-*\\(return\\|stop\\|suicide\\)"
+;;  "A regex to match the ends of blocks for serpent-indent-calculate-indentation")
+
+(defun serpent-indent-calculate-indentation ()
+  "Calculate correct indentation offset for the current line."
+  (interactive)
+  (let ((indent 0))
+    (save-excursion
+      (beginning-of-line)
+      ;; On a block start line? Don't use its indentation
+      (when (and (looking-at serpent-block-start)
+                 (not (bobp)))
+        (forward-line -1))
+      ;; Scan back looking for the start of the block
+      (while (and (not (looking-at serpent-block-start))
+                  (not (bobp)))
+        (forward-line -1))
+        ;; search-forward updates point, so save excursion
+        (setf indent (count-matches "\\s-"
+                                    (line-beginning-position)
+                                    (re-search-forward "[^[:space:]]" (line-end-position)))))
+    (save-excursion
+      (beginning-of-line)
+      ;; Don't indent for else
+      (when (not (looking-at "\\s-*\\(init\\|code\\|else\\)"))
+        (incf indent serpent-indent-offset)))
+    indent))
+
+(defun serpent-indent-calculate-levels ()
+  "Calculate `serpent-indent-levels' and reset `serpent-indent-current-level'."
+  (let* ((indentation (serpent-indent-calculate-indentation))
+         (remainder (% indentation serpent-indent-offset))
+         (steps (/ (- indentation remainder) serpent-indent-offset)))
+    (setq serpent-indent-levels (list 0))
+    (dotimes (step steps)
+      (push (* serpent-indent-offset (1+ step)) serpent-indent-levels))
+    (when (not (eq 0 remainder))
+      (push (+ (* serpent-indent-offset steps) remainder) serpent-indent-levels))
+    (setq serpent-indent-levels (nreverse serpent-indent-levels))
+    (setq serpent-indent-current-level (1- (length serpent-indent-levels)))))
+
+(defun serpent-indent-toggle-levels ()
+  "Toggle `serpent-indent-current-level' over `serpent-indent-levels'."
+  (setq serpent-indent-current-level (1- serpent-indent-current-level))
+  (when (< serpent-indent-current-level 0)
+    (setq serpent-indent-current-level (1- (length serpent-indent-levels)))))
+
+(defun serpent-indent-line (&optional force-toggle)
+  "Internal implementation of `serpent-indent-line-function'.
+Uses the offset calculated in
+`serpent-indent-calculate-indentation' and available levels
+indicated by the variable `serpent-indent-levels' to set the
+current indentation."
+  (or
+   (and (or (and (memq this-command serpent-indent-trigger-commands)
+                 (eq last-command this-command))
+            force-toggle)
+    (not (equal serpent-indent-levels '(0)))
+    (or (serpent-indent-toggle-levels) t))
+   (serpent-indent-calculate-levels))
+  (let* ((starting-pos (point-marker))
+         (indent-ending-position
+          (+ (line-beginning-position) (current-indentation)))
+         (follow-indentation-p
+          (or (bolp)
+              (and (<= (line-beginning-position) starting-pos)
+                   (>= indent-ending-position starting-pos))))
+         (next-indent (nth serpent-indent-current-level serpent-indent-levels)))
+    (unless (= next-indent (current-indentation))
+      (beginning-of-line)
+      (delete-horizontal-space)
+      (indent-to next-indent)
+      (goto-char starting-pos))
+    (and follow-indentation-p (back-to-indentation)))
+  ;;(serpent-info-closing-block-message)
+  )
+
+(defun serpent-indent-line-function ()
+  "`indent-line-function' for Serpent mode.
+See `serpent-indent-line' for details."
+  (serpent-indent-line))
+
+(defun serpent-indent-dedent-line ()
+  "De-indent current line."
+  (interactive "*")
+  ;;(when (and (not (serpent-syntax-comment-or-string-p))
+  ;;           (<= (point-marker) (save-excursion
+  ;;                                (back-to-indentation)
+  ;;                                (point-marker)))
+  ;;           (> (current-column) 0))
+    (serpent-indent-line t)
+    t);;)
+
+(defun serpent-indent-dedent-line-backspace (arg)
+  "De-indent current line.
+Argument ARG is passed to `backward-delete-char-untabify' when
+point is  not in between the indentation."
+  (interactive "*p")
+  (when (not (serpent-indent-dedent-line))
+    (backward-delete-char-untabify arg)))
+(put 'serpent-indent-dedent-line-backspace 'delete-selection 'supersede)
+
+(defun serpent-indent-shift-left (start end &optional count)
+  "Shift lines contained in region START END by COUNT columns to the left.
+COUNT defaults to `serpent-indent-offset'.  If region isn't
+active, the current line is shifted.  The shifted region includes
+the lines in which START and END lie.  An error is signaled if
+any lines in the region are indented less than COUNT columns."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end) current-prefix-arg)
+     (list (line-beginning-position) (line-end-position) current-prefix-arg)))
+  (if count
+      (setq count (prefix-numeric-value count))
+    (setq count serpent-indent-offset))
+  (when (> count 0)
+    (let ((deactivate-mark nil))
+      (save-excursion
+        (goto-char start)
+        (while (< (point) end)
+          (if (and (< (current-indentation) count)
+                   (not (looking-at "[ \t]*$")))
+              (error "Can't shift all lines enough"))
+          (forward-line))
+        (indent-rigidly start end (- count))))))
+(add-to-list 'debug-ignored-errors "^Can't shift all lines enough")
+
+(defun serpent-indent-shift-right (start end &optional count)
+  "Shift lines contained in region START END by COUNT columns to the left.
+COUNT defaults to `serpent-indent-offset'.  If region isn't
+active, the current line is shifted.  The shifted region includes
+the lines in which START and END lie."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end) current-prefix-arg)
+     (list (line-beginning-position) (line-end-position) current-prefix-arg)))
+  (let ((deactivate-mark nil))
+    (if count
+        (setq count (prefix-numeric-value count))
+      (setq count serpent-indent-offset))
+    (indent-rigidly start end count)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Compilation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defcustom serpent-executable "serpent"
+  "The command to call to compile .se files."
+  :type 'boolean
+  :group 'serpent-mode)
+
+(defun serpent-buffer-file-command (command)
+  "Call the serpent executable on the file for the current buffer."
+  (interactive)
+  (let ((output-buffer (get-buffer-create "*Serpent*"))
+        (filename (buffer-file-name)))
+    (with-current-buffer output-buffer
+      (erase-buffer))
+    (cond
+     ((not filename)
+      (message "Cannot use non-file buffer."))
+     ((buffer-modified-p)
+      (message "Please save changes before compiling."))
+     (t (shell-command
+         (concat serpent-executable " " command " \"" (buffer-file-name) "\"")
+         output-buffer)))))
+
+(defun serpent-compile-to-assembly-current-buffer ()
+  (interactive)
+  (serpent-buffer-file-command "compile_to_assembly"))
+
+(defun serpent-compile-current-buffer ()
+  (interactive)
+  (serpent-buffer-file-command "compile"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Syntax table
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar serpent-syntax-table
+  (let ((st (make-syntax-table)))
+    ;; Dots are valid word constituents
+    (modify-syntax-entry ?. "_" st)
+    ;; Colons are whitespace
+    (modify-syntax-entry ?: "-" st)
+    ;; c++-style comments: //
+    (modify-syntax-entry ?\/ ". 12b" st)
+    (modify-syntax-entry ?\n "> b" st)
+    st)
+  "Syntax table for Serpent major mode.")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Keymap
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconst serpent-mode-map (make-keymap)
+  "Keymap for Serpent major mode.")
+(define-key serpent-mode-map (kbd "C-x C-a") 'serpent-compile-to-assembly-current-buffer)
+(define-key serpent-mode-map (kbd "C-x C-e") 'serpent-compile-current-buffer)
+;; Indent specific
+(define-key serpent-mode-map "\177" 'serpent-indent-dedent-line-backspace)
+(define-key serpent-mode-map (kbd "<backtab>") 'serpent-indent-dedent-line)
+(define-key serpent-mode-map "\C-c<" 'serpent-indent-shift-left)
+(define-key serpent-mode-map "\C-c>" 'serpent-indent-shift-right)
+(define-key serpent-mode-map ":" 'serpent-indent-electric-colon)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define the mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;###autoload
+(define-derived-mode serpent-mode prog-mode "serpent"
+  "Major mode for editing Serpent programming language files.
+\\{serpent-mode-map}"
+  :group 'serpent-mode
+  :syntax-table serpent-syntax-table
+  :keymap serpent-mode-map
+  (set (make-local-variable 'font-lock-defaults)
+       '(serpent-font-lock-keywords))
+  (set (make-local-variable 'comment-start) "//")
+  (set (make-local-variable 'comment-end) "")
+  (setq-local indent-line-function 'serpent-indent-line))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Provide the mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provide 'serpent-mode)
